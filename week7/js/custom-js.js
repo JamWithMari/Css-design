22edd90433c365e2f96c9cf43ecf27df401f96b7
JamWithMari <jamaricharles@duck.com>
2025-03-04 16:56:15 -0500

    updated

---
+++ week7/js/custom-js.js
@@ -0,0 +1,7 @@
+$(window).on("scroll", function (){
+   if($(this).scrollTop() > 100){
+       $("header").addClass("scrolledHeader");
+   }else{
+       $("header").removeClass("scrolledHeader");
+   }
+});
